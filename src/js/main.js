e4ed8c262f2760aa5e796bc86080b5914404181d
Bohdan <zamchenco@gmail.com>
2017-05-28 18:47:36 +0300

    main.js fix

--- src/js/main.js
+++ src/js/main.js
@@ -16,69 +16,120 @@
 
 jQuery(document).ready(function($) {
 
-    console.log("ready!");
+  /**
+   * detect IE
+   * returns version of IE or false, if browser is not Internet Explorer
+   */
+  function detectIE() {
+    var ua = window.navigator.userAgent;
 
-    //globals
-    let $window = $(window),
-        $doc = $('html'),
-        $body = $('body');
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
 
-        // //get scroll width
-        //
-        // function scrollWidth(){
-        //     let div = $('<div>').css({
-        //         position: "absolute",
-        //         top: "0px",
-        //         left: "0px",
-        //         width: "100px",
-        //         height: "100px",
-        //         visibility: "hidden",
-        //         overflow: "scroll"
-        //     });
-        //     $('body').eq(0).append(div);
-        //     let width = div.get(0).offsetWidth - div.get(0).clientWidth;
-        //     div.remove();
-        //     return width;
-        // }
-        //
-        // let scrollWidth = scrollWidth();
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
 
-        // close by outer click
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
 
-        // $doc.mouseup(function (e) {
-        //   closeOut(e, elem, closeClass);
-        // });
+    // other browser
+    return false;
+  }
 
-        function closeOut(e, elem, closeClass) {
-          // e - event
-          // elem - jquery element
-          // closeClass - class tat hide element
+  var ieVersion = detectIE();
+  // console.log(ieVersion);
 
-          if (elem.hasClass(closeClass)) {
-            return;
-          }
-          if (!elem.is(e.target) // click outer element
-              && elem.has(e.target).length === 0) { // and not his children
-            elem.addClass(closeClass); // hide him
-          }
-        }
+  //////////////////////////////////////////////////// Mediaquery
+  // window.matchMedia("(max-width: 400px)").matches
 
 
-        //scroll to anchor
-        let btnAnchor = $('.btnAnchor');
-        $doc.on('click', '.btnAnchor', scrollToAnchor);
+  console.log("ready!");
 
-        function scrollToAnchor(e) {
-          e.preventDefault();
-          let id  = $(this).data('href'),
-              top = $(id).offset().top;
 
-          $('body,html').animate({scrollTop: top}, 500);
-        };
+  //get max height
+  function getMax(elems) {
+    var max = elems.eq(0).outerHeight();
+    elems.each(function () {
+      max = ( $(this).outerHeight() > max ) ? $(this).outerHeight() : max;
+    });
+    return max;
+  }
 
-    //nav
+  
+  //globals
+  let $window = $(window),
+    $doc = $('html'),
+    $body = $('body');
 
-    //debounce
+  // //get scroll width
+  //
+  // function scrollWidth(){
+  //     let div = $('<div>').css({
+  //         position: "absolute",
+  //         top: "0px",
+  //         left: "0px",
+  //         width: "100px",
+  //         height: "100px",
+  //         visibility: "hidden",
+  //         overflow: "scroll"
+  //     });
+  //     $('body').eq(0).append(div);
+  //     let width = div.get(0).offsetWidth - div.get(0).clientWidth;
+  //     div.remove();
+  //     return width;
+  // }
+  //
+  // let scrollWidth = scrollWidth();
+
+  // close by outer click
+
+  // $doc.mouseup(function (e) {
+  //   closeOut(e, elem, closeClass);
+  // });
+
+  function closeOut(e, elem, closeClass) {
+    // e - event
+    // elem - jquery element
+    // closeClass - class tat hide element
+
+    if (elem.hasClass(closeClass)) {
+      return;
+    }
+    if (!elem.is(e.target) // click outer element
+      &&
+      elem.has(e.target).length === 0) { // and not his children
+      elem.addClass(closeClass); // hide him
+    }
+  }
+
+
+  //scroll to anchor
+  let btnAnchor = $('.btnAnchor');
+  $doc.on('click', '.btnAnchor', scrollToAnchor);
+
+  function scrollToAnchor(e) {
+    e.preventDefault();
+    let id = $(this).data('href'),
+      top = $(id).offset().top;
+
+    $('body,html').animate({
+      scrollTop: top
+    }, 500);
+  };
+
+  //nav
+
+  //debounce
   //   let scrollTimeout;
   //   window.addEventListener('scroll', function(evt){
   //     clearTimeout(scrollTimeout);
@@ -87,36 +138,36 @@ jQuery(document).ready(function($) {
   //     }, 100);
   //  });
 
-    //sticky nav
-    // $(window).scroll(function() {
-    //
-    //
-    //     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    //     let nav = $('.nav');
-    //     if (nav.length != 0) {
-    //       if (scrolled >= 250) {
-    //           nav.addClass('fixed');
-    //       }
-    //       if (scrolled >= 350) {
-    //           nav.addClass('shown');
-    //       }
-    //       if (scrolled <= 300) {
-    //           nav.removeClass('shown');
-    //           nav.removeClass('fixed');
-    //       }
-    //     }
-    // });
+  //sticky nav
+  // $(window).scroll(function() {
+  //
+  //
+  //     let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  //     let nav = $('.nav');
+  //     if (nav.length != 0) {
+  //       if (scrolled >= 250) {
+  //           nav.addClass('fixed');
+  //       }
+  //       if (scrolled >= 350) {
+  //           nav.addClass('shown');
+  //       }
+  //       if (scrolled <= 300) {
+  //           nav.removeClass('shown');
+  //           nav.removeClass('fixed');
+  //       }
+  //     }
+  // });
 
-    let btnToggleNav = $('.toggle-nav');
+  let btnToggleNav = $('.toggle-nav');
 
-    btnToggleNav.on('click', function (e) {
-      e.preventDefault();
-      $(this).toggleClass('opened');
-      $($(this).data('href')).toggleClass('closed');
-    });
+  btnToggleNav.on('click', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('opened');
+    $($(this).data('href')).toggleClass('closed');
+  });
 
-    //modals
-    let modalLink = $('.modal-link');
+  //modals
+  let modalLink = $('.modal-link');
 
   //   modalLink.magnificPopup({
   //     type: 'inline',
@@ -135,23 +186,23 @@ jQuery(document).ready(function($) {
   // });
 
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////MASKEDINPUT jQuery
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////MASKEDINPUT jQuery
 
-    // $.mask.definitions['~']='[+-]';
-    //  $('#date').mask('99/99/9999',{
-    //  	placeholder:"mm/dd/yyyy"
-    //  });
-    //  $('#phone').mask('(999) 999-9999');
-    //  $('#phoneext').mask("(999) 999-9999? x99999");
-    //  $("#tin").mask("99-9999999");
-    //  $("#ssn").mask("999-99-9999");
-    //  $("#product").mask("a*-999-a999",{
-    //  	placeholder:" ",
-    //  	completed:function(){
-    //  		alert("You typed the following: "+this.val());
-    //  	}
-    //  });
-    //  $("#eyescript").mask("~9.99 ~9.99 999");
+  // $.mask.definitions['~']='[+-]';
+  //  $('#date').mask('99/99/9999',{
+  //  	placeholder:"mm/dd/yyyy"
+  //  });
+  //  $('#phone').mask('(999) 999-9999');
+  //  $('#phoneext').mask("(999) 999-9999? x99999");
+  //  $("#tin").mask("99-9999999");
+  //  $("#ssn").mask("999-99-9999");
+  //  $("#product").mask("a*-999-a999",{
+  //  	placeholder:" ",
+  //  	completed:function(){
+  //  		alert("You typed the following: "+this.val());
+  //  	}
+  //  });
+  //  $("#eyescript").mask("~9.99 ~9.99 999");
 
 
 
@@ -163,9 +214,9 @@ jQuery(document).ready(function($) {
 
 /////////////////////vendors
 /*
-* = devs/scroll-top.js
-* = devs/vanilla-scroll-top.js
-* */
+ * = devs/scroll-top.js
+ * = devs/vanilla-scroll-top.js
+ * */
 
 
 ////////////////////////////////////////////////////////////////////////////////////////replace span and gaps in href
